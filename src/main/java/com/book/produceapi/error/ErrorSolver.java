6bfacb1ef4e1576a71b59d1b4f767f3d769300e5
avrm <avrmeduard@gmail.com>
2020-09-01 09:38:33 +0300

    slightly modified

--- src/main/java/com/book/produceapi/error/ErrorSolver.java
+++ src/main/java/com/book/produceapi/error/ErrorSolver.java
@@ -4,6 +4,7 @@ import com.book.produceapi.model.errormodel.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,6 +19,7 @@ public class ErrorSolver {
         ErrorResponse response = new ErrorResponse();
         response.setErrorDescription(exception.getMessage());
         httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
         return response;
     }
 
