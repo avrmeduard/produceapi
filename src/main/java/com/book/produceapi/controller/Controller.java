75468d5eda914d2fd862a564cc617e070d6ff49c
avrm <avrmeduard@gmail.com>
2020-09-01 22:08:18 +0300

    Api entity added

--- src/main/java/com/book/produceapi/controller/Controller.java
+++ src/main/java/com/book/produceapi/controller/Controller.java
@@ -2,7 +2,12 @@ package com.book.produceapi.controller;
 
 import com.book.produceapi.model.addbook.AddBookRequest;
 import com.book.produceapi.model.addbook.AddBookResponse;
+import com.book.produceapi.model.errormodel.ErrorResponse;
 import com.book.produceapi.model.getbook.GetBookResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +17,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Optional;
 
+@Api(value = "getBook / addBook",
+     description = "Rest API for book")
 public interface Controller {
+
 
     @GetMapping("/getBook")
     GetBookResponse getBook(@RequestParam Optional<Integer> bookId,
