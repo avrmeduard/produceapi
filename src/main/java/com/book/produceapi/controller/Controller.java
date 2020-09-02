9279d3dccd516f62698a950c6662add3ec9f10a3
avrm <avrmeduard@gmail.com>
2020-09-02 20:01:25 +0300

    updateBook HttpVerb modified in PUT

--- src/main/java/com/book/produceapi/controller/Controller.java
+++ src/main/java/com/book/produceapi/controller/Controller.java
@@ -11,10 +11,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -124,7 +121,7 @@ public interface Controller {
                            @ApiResponse(code = HttpServletResponse.SC_METHOD_NOT_ALLOWED,
                                         message = "Method not allowed",
                                         response = ErrorResponse.class) })
-    @PostMapping("/updateBook")
+    @PutMapping("/updateBook")
     ResponseEntity<UpdateBookResponse> updateBook(@RequestBody @Valid UpdateBookRequest updateBookRequest,
                                                                       HttpServletResponse httpServletResponse);
 
